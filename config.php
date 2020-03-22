b0760749bc90efcf8a6513b3a80c33140b8ed8aa
Alvaro Castaño <alvarocastano6@gmail.com>
2020-03-22 00:22:53 -0500

    Se quita el espacio

--- config.php
+++ config.php
@@ -9,7 +9,6 @@ define('DB_NAME', 'alumnos');
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
- 
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
