62a31ea7a3ee168a0cc0c3cfdf6b6016895effb5
Alvaro-PC\Alvaro <alvarocastano6@gmail.com>
2020-03-21 23:46:23 -0500

    CRUD básico PHP

---
+++ create.php
@@ -0,0 +1,118 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$nombre = $ciudad = $telefono = "";
+$nombre_err = $ciudad_err = $telefono_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validar nombre
+    $input_nombre = trim($_POST["nombre"]);
+    if(empty($input_nombre)){
+        $nombre_err = "Ingrese nombre.";
+    } elseif(!filter_var($input_nombre, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $nombre_err = "Ingrese un nombre valido.";
+    } else{
+        $nombre = $input_nombre;
+    }
+    
+    // Validar ciudad
+    $input_ciudad = trim($_POST["ciudad"]);
+    if(empty($input_ciudad)){
+        $ciudad_err = "Ingrese ciudad.";     
+    } else{
+        $ciudad = $input_ciudad;
+    }
+    
+    // Validar telefono
+    $input_telefono = trim($_POST["telefono"]);
+    if(empty($input_telefono)){
+        $telefono_err = "Ingrese telefono.";     
+    } elseif(!ctype_digit($input_telefono)){
+        $telefono_err = "Ingrese telefono valido.";
+    } else{
+        $telefono = $input_telefono;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($nombre_err) && empty($ciudad_err) && empty($telefono_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO alumno (nombre, ciudad, telefono) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_nombre, $param_ciudad, $param_telefono);
+            
+            // Set parameters
+            $param_nombre = $nombre;
+            $param_ciudad = $ciudad;
+            $param_telefono = $telefono;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Create Record</h2>
+                    </div>
+                    <p>Please fill this form and submit to add employee record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($nombre_err)) ? 'has-error' : ''; ?>">
+                            <label>Nombre</label>
+                            <input type="text" name="nombre" class="form-control" value="<?php echo $nombre; ?>">
+                            <span class="help-block"><?php echo $nombre_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($ciudad_err)) ? 'has-error' : ''; ?>">
+                            <label>Ciudad</label>
+                            <textarea name="ciudad" class="form-control"><?php echo $ciudad; ?></textarea>
+                            <span class="help-block"><?php echo $ciudad_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($ciudad_err)) ? 'has-error' : ''; ?>">
+                            <label>Telefono</label>
+                            <input type="text" name="telefono" class="form-control" value="<?php echo $telefono; ?>">
+                            <span class="help-block"><?php echo $telefono_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-default">Cancelar</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD PHP</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left">Detalle de Alumnos</h2>
+                        <a href="create.php" class="btn btn-success pull-right">Agregar Nuevo Alumno</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+                    
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM alumno";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>Nombre</th>";
+                                        echo "<th>Ciudad</th>";
+                                        echo "<th>Acciones</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['nombre'] . "</td>";
+                                        echo "<td>" . $row['ciudad'] . "</td>";
+                                        echo "<td>";
+                                            echo "<a href='read.php?id=". $row['id'] ."' title='Ver Registro' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                            echo "<a href='update.php?id=". $row['id'] ."' title='Actualizar Registro' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            echo "<a href='delete.php?id=". $row['id'] ."' title='Eliminar Registro' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                    }
+ 
+                    // Close connection
+                    mysqli_close($link);
+                    ?>
+
+                    <p>Ingrese información en los campos para filtrar</p>
+                    <form action="filter.php" method="post">
+                        <div class="form-group">
+                            <label>Nombre</label>
+                            <input type="text" name="nombre" class="form-control">
+                            <span class="help-block"></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Seleccionar</label>
+                            <select class="form-control" name="orden">
+                                <option value="1">Ascendente</option>
+                                <option value="2">Descendente</option>
+                            </select>
+                        </div>     
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
