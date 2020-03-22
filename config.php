489c0647c3402be1a9f94cf47f912199c5af70e8
Alvaro Castaño <alvarocastano6@gmail.com>
2020-03-22 00:18:37 -0500

    Espacio

--- config.php
+++ config.php
@@ -5,9 +5,10 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'alumnos');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
  
 // Check connection
 if($link === false){
